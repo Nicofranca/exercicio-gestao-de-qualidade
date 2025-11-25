581a0efa5491eaf9edd798fceeda87ee22036dca
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2025-11-25 18:12:16 -0300

    feat: terminar falha repository

--- src/main/java/org/example/repository/falha/FalhaRepository.java
+++ src/main/java/org/example/repository/falha/FalhaRepository.java
@@ -1,4 +1,41 @@
 package org.example.repository.falha;
 
+import org.example.database.Conexao;
+import org.example.model.Falha;
+
+import java.sql.*;
+
 public class FalhaRepository {
+
+    public Falha saveNewFalha(Falha falha) throws SQLException{
+        String query = """
+                INSERT INTO Falha(equipamentoId, dataHoraOcorrencia, descricao, criticidade, status, tempoParadaHoras) VALUES (?, ?, ?, ?, ?, ?)
+                """;
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setLong(1, falha.getEquipamentoId());
+            stmt.setTimestamp(2, Timestamp.valueOf(falha.getDataHoraOcorrencia()));
+
+        }
+    }
+
+
+
+
+    /*private static final String SQL_CREATE_TABLE_FALHA =
+            """
+            CREATE TABLE IF NOT EXISTS Falha (
+                id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                equipamentoId BIGINT NOT NULL,
+                dataHoraOcorrencia DATETIME NOT NULL,
+                descricao TEXT NOT NULL,
+                criticidade VARCHAR(50) NOT NULL,
+                status VARCHAR(50) NOT NULL,
+                tempoParadaHoras DECIMAL(10,2) DEFAULT 0.00,
+
+                CONSTRAINT chk_criticidade_falha CHECK (criticidade IN ('BAIXA','MEDIA','ALTA','CRITICA')),
+                CONSTRAINT chk_status_falha CHECK (status IN ('ABERTA','EM_ANDAMENTO','RESOLVIDA'))
+            );
+            """;*/
 }
