c99aa379e0dbe70d8e9cc1076a7782bc55fce876
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2025-11-27 18:22:46 -0300

    feat: saveNewFalha - Repository

--- src/main/java/org/example/repository/falha/FalhaRepository.java
+++ src/main/java/org/example/repository/falha/FalhaRepository.java
@@ -5,6 +5,8 @@ import org.example.model.Falha;
 
 import java.sql.*;
 
+import static java.lang.String.valueOf;
+
 public class FalhaRepository {
 
     public Falha saveNewFalha(Falha falha) throws SQLException{
@@ -16,8 +18,30 @@ public class FalhaRepository {
             PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
             stmt.setLong(1, falha.getEquipamentoId());
             stmt.setTimestamp(2, Timestamp.valueOf(falha.getDataHoraOcorrencia()));
+            stmt.setString(3, falha.getDescricao());
+            stmt.setString(4, falha.getCriticidade());
+            stmt.setString(5, falha.getStatus());
+            stmt.setBigDecimal(6, falha.getTempoParadaHoras());
+
+            stmt.executeUpdate();
+
+            try(ResultSet rs = stmt.getGeneratedKeys()) {
+                if (rs.next()){
+                    falha.setId(rs.getLong(1));
+                    falha.setEquipamentoId(rs.getLong("equipamentoId"));
+                    falha.setDataHoraOcorrencia(rs.getTimestamp(valueOf(Timestamp.valueOf("dataHoraOcorrencia"))).toLocalDateTime());
+                    falha.setDescricao((rs.getString("descricao")));
+                    falha.setCriticidade(rs.getString("criticidade"));
+                    falha.setStatus(rs.getString("status"));
+                    falha.setTempoParadaHoras(rs.getBigDecimal("tempoParadaHoras"));
+
+                    return falha;
+                }
+            }
 
         }
+
+        return null;
     }
 
 
